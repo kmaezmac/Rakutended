983da268274f198d67c0c776f362c5e10ef22266
ktrmzm <k.maezmac@gmail.com>
2024-05-21 16:23:41 +0900

    help

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 import express from "express";
 import process from "process";
 import axios from "axios";
-import https from 'https';
 
 const app = express();
 
@@ -16,30 +15,24 @@ const rakuten = async () => {
     var requestUrl = "https://app.rakuten.co.jp/services/api/IchibaItem/Ranking/20220601?applicationId=" + process.env.RAKUTEN_APP_ID
         + "&age=" + age + "&sex=1&carrier=0&page=" + random + "&affiliateId=" + process.env.RAKUTEN_AFFILIATE_ID;
     console.log(requestUrl);
-    https.get(requestUrl, (resp) =>{
-        let response = ''; 
-        resp.on('data', (chunk) => { 
-            response += chunk; 
-        }); 
-        resp.on('end', () => {
-            if (response.status !== 201) {
-                var randomNo = Math.floor(Math.random() * (response.Items.length));
-                var itemName = response.Items[randomNo].Item.itemName;
-                var catchcopy = response.Items[randomNo].Item.catchcopy;
-                var affiliateUrl = response.Items[randomNo].Item.affiliateUrl;
-                console.log(itemName);
-                console.log(catchcopy);
-                console.log(affiliateUrl);
-                var tweetText = (itemName + catchcopy).substring(0, 90) + " " + affiliateUrl + " #楽天ROOM #楽天 #楽天市場 #ad #PR";
-                console.log(tweetText);
-                return tweetText;
-            }
-        }); 
-    
-    }).on("error", (err) => { 
-        console.log("Error: " + err.message); 
-        return false;
-    })
+    axios.get(requestUrl, {
+    }).then(async (response) => {
+        if (response.status !== 201) {
+            var randomNo = Math.floor(Math.random() * (response.data.Items.length));
+            var itemName = response.data.Items[randomNo].Item.itemName;
+            var catchcopy = response.data.Items[randomNo].Item.catchcopy;
+            var affiliateUrl = response.data.Items[randomNo].Item.affiliateUrl;
+            console.log(itemName);
+            console.log(catchcopy);
+            console.log(affiliateUrl);
+            var tweetText = itemName + catchcopy
+            return tweetText.substring(0, 90) + " " + affiliateUrl + " #楽天ROOM #楽天 #楽天市場 #ad #PR";
+        }
+    }).catch((error) => {
+        console.log(error);
+        return;
+    });
+
 };
 
 
